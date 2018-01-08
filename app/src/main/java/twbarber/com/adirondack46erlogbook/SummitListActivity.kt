63ff7b2cb89765b93150e6e44d39409f0c547344
Tyler Barber <dev@twbarber.com>
2018-01-07 19:31:54 -0500

    Initial Commit

---
+++ app/src/main/java/twbarber/com/adirondack46erlogbook/SummitListActivity.kt
@@ -0,0 +1,12 @@
+package twbarber.com.adirondack46erlogbook
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SummitListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summt_list)
+    }
+}
